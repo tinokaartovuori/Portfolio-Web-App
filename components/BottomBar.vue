77353f0673ad2a3e80b9f4256dc75c213ee37142
tinokaartovuori <tino.kaartovuori@gmail.com>
2023-05-31 22:36:13 +0300

    feat: scroll to discover hides when scrolled down

--- components/BottomBar.vue
+++ components/BottomBar.vue
@@ -1,5 +1,25 @@
 <template>
   <Bar class="fixed bottom-8 sm:bottom-12 md:bottom-16">
-    <ScrollToDiscover />
+    <ScrollToDiscover :showComponent="scrollToDiscoverVisible" />
   </Bar>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useWindowSize } from '@vueuse/core'
+import { storeToRefs } from 'pinia'
+import { useScrollStateStore } from '~/store/scrollState'
+const scrollStateStore = useScrollStateStore()
+const { scrollY } = storeToRefs(scrollStateStore)
+
+const scrollToDiscoverVisible = ref(true)
+const { width, height } = useWindowSize()
+
+watch(scrollY, () => {
+  if (scrollY.value > height.value / 3) {
+    scrollToDiscoverVisible.value = false
+  } else {
+    scrollToDiscoverVisible.value = true
+  }
+})
+</script>
--- components/ScrollToDiscover.vue
+++ components/ScrollToDiscover.vue
@@ -1,17 +1,27 @@
 <template>
-  <!-- TODO: Here we need to add the press event that starts scrolling the page -->
-  <div class="flex">
-    <WaveText
-      text="Scroll to discover"
-      class="xs:text-base text-onyx dark:text-platinum text-sm font-light sm:text-xl md:text-2xl"
-    />
-    <div
-      class="up-and-down xs:text-base text-onyx dark:text-platinum px-2 text-sm font-light sm:text-xl md:text-2xl"
-    >
-      ↓
+  <transition name="fade">
+    <div v-show="showComponent" class="flex">
+      <WaveText
+        text="Scroll to discover"
+        class="text-sm font-light text-onyx dark:text-platinum xs:text-base sm:text-xl md:text-2xl"
+      />
+      <div
+        class="up-and-down px-2 text-sm font-light text-onyx dark:text-platinum xs:text-base sm:text-xl md:text-2xl"
+      >
+        ↓
+      </div>
     </div>
-  </div>
+  </transition>
 </template>
+
+<script setup>
+defineProps({
+  showComponent: {
+    type: Boolean,
+    required: false,
+  },
+})
+</script>
 
 <style scoped>
 .up-and-down {
@@ -29,5 +39,18 @@
   50% {
     transform: translateY(3px);
   }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter-from /* .fade-enter in < 2.1.8 */ {
+  opacity: 0;
+}
+
+.fade-leave-to /* .fade-leave-active in < 2.1.8 */ {
+  opacity: 0;
 }
 </style>
